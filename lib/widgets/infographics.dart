2a1dafe71f45a01abdf192494b4b35ea8299b115
Jack Scherlag <jackscherlag@Jacks-MacBook-Pro.local>
2020-08-04 20:27:59 -0500

    Initial commit

---
+++ lib/widgets/infographics.dart
@@ -0,0 +1,79 @@
+import "package:flutter/cupertino.dart";
+import "package:flutter/services.dart" show rootBundle;
+import "package:auto_size_text/auto_size_text.dart";
+
+class Infographics extends StatefulWidget {
+  @override
+  _InfographicsState createState() => _InfographicsState();
+}
+
+class _InfographicsState extends State<Infographics> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          color: CupertinoColors.white,
+          height: MediaQuery.of(context).size.height * 0.9,
+          width: MediaQuery.of(context).size.width * 0.5,
+        ),
+        Positioned.fill(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height * 0.25,
+                width: MediaQuery.of(context).size.width * 0.16,
+                decoration: BoxDecoration(
+                  color: CupertinoColors.black,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: AutoSizeText(
+                    ">600 Flight Hours",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: CupertinoColors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.25,
+                width: MediaQuery.of(context).size.width * 0.16,
+                child: Center(
+                  child: AutoSizeText(
+                    "1 US Patent",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: CupertinoColors.white),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: CupertinoColors.black,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.25,
+                width: MediaQuery.of(context).size.width * 0.16,
+                child: Center(
+                  child: AutoSizeText(
+                    "Multi-instrumentalist",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: CupertinoColors.white,
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: CupertinoColors.black,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/text_screen.dart
@@ -0,0 +1,30 @@
+import "package:flutter/cupertino.dart";
+import "package:flutter/services.dart" show rootBundle;
+import "package:auto_size_text/auto_size_text.dart";
+
+class TextScreen extends StatefulWidget {
+  @override
+  List<String> texts;
+  TextScreen({this.texts});
+  _TextScreenState createState() => _TextScreenState();
+}
+
+class _TextScreenState extends State<TextScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: CupertinoColors.black,
+      height: MediaQuery.of(context).size.height * 0.9,
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: Center(
+        child: AutoSizeText(
+          "${widget.texts[0]}",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: CupertinoColors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
