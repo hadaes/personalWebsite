461e556a3e4a666f0fb2f246d19e26e8a9c43b13
Jack Scherlag <jackscherlag@Jacks-MBP.attlocal.net>
2020-09-02 16:35:41 -0500

    Added animations to text and background

--- lib/screens/aboutme.dart
+++ lib/screens/aboutme.dart
@@ -9,21 +9,41 @@ class AboutMe extends StatefulWidget {
   _AboutMeState createState() => _AboutMeState();
 }
 
-class _AboutMeState extends State<AboutMe> {
+class _AboutMeState extends State<AboutMe> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 8));
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  // add layout builder
   @override
   Widget build(BuildContext context) {
-    if (widget.buttonPushed > 0) {
-      return Center(
-        child: Text(
-          "I am jack scherlag",
-          style: TextStyle(fontSize: 30, color: Colors.white),
-        ),
-      );
-    } else {
-      return Container(
-        height: 0,
-        width: 0,
-      );
-    }
+    _controller.forward();
+    return AnimatedContainer(
+      color: widget.buttonPushed == 0
+          ? Colors.transparent
+          : Colors.transparent.withOpacity(0.3),
+      duration: Duration(seconds: 1),
+      child: widget.buttonPushed == 0
+          ? Container()
+          : Center(
+              child: FadeTransition(
+                opacity: _animation,
+                child: Text(
+                  "I am jack scherlag",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: widget.buttonPushed == 0
+                          ? Colors.transparent
+                          : Colors.white),
+                ),
+              ),
+            ),
+    );
   }
 }
