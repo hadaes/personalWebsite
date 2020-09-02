d4c408ceb707e5fb19307a0b8157e2b15e14242b
Jack Scherlag <jackscherlag@Jacks-MBP.attlocal.net>
2020-09-02 17:00:20 -0500

    Drawer goes away when button pushed

--- lib/home.dart
+++ lib/home.dart
@@ -80,6 +80,7 @@ class _HomeState extends State<Home> {
                           child: GestureDetector(
                             onTap: () {
                               print("clicked");
+                              Navigator.pop(context);
                             },
                             child: AutoSizeText(
                               "About This Site",
