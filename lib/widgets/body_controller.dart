6272e6381be9d0f4255419d5e9cae3136b03be18
Jack Scherlag <jackscherlag@Jacks-MBP.attlocal.net>
2020-09-04 19:48:30 -0500

    Added widget to control body content

---
+++ lib/widgets/body_controller.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+import 'package:personalWebsite/screens/aboutme.dart';
+import 'package:personalWebsite/screens/site.dart';
+
+class BodyController extends StatelessWidget {
+  final int buttonPushed;
+
+  BodyController(this.buttonPushed);
+
+  @override
+  Widget build(BuildContext context) {
+    if (buttonPushed == 0) {
+      return Container();
+    } else if (buttonPushed == 1) {
+      return AboutMe(buttonPushed);
+    } else {
+      return Site(buttonPushed);
+    }
+  }
+}
