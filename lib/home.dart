5e9ea176bc42dfa9a1ca30a8982000f22666741f
Jack Scherlag <jack.scherlag@gmail.com>
2021-08-14 22:12:33 -0500

    Section name change

--- lib/home.dart
+++ lib/home.dart
@@ -229,7 +229,7 @@ class _HomeState extends State<Home> {
                                     Navigator.pop(context);
                                   },
                                   child: AutoSizeText(
-                                    "Projects",
+                                    "Projects/Experience",
                                     presetFontSizes: [22, 20, 15, 14],
                                     maxLines: 2,
                                     group: myGroup,
