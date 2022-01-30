b11f46b8d57072474d8d0f3f150b5d73950fd1e4
Jack Scherlag <jack.scherlag@gmail.com>
2022-01-30 01:00:44 -0500

    Added thesis. Working on getting text to cooperate

--- lib/screens/Projects/desktop.dart
+++ lib/screens/Projects/desktop.dart
@@ -15,8 +15,75 @@ class ProjectsDesktop extends StatelessWidget {
         ),
         Center(
           child: AutoSizeText(
+            "Senior Thesis\nStabilizing Metal Anodes in Perovskite LEDs",
+            group: titleGroupDesktop,
+            stepGranularity: 1.0,
+            style: TextStyle(
+              fontSize: 30,
+              fontFamily: "Poppins",
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image(
+              image: AssetImage("assets/images/thesis_poster.png"),
+              width: MediaQuery.of(context).size.width * 0.5,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: AutoSizeText(
+                "My senior thesis focuses on stabilizing and improving perovskite LEDS (PeLEDs). Bottom-emitting PeLEDS (BEPLEDS) are more stable than top-emitting PeLEDS (TEPLEDs) but have a lower theoretical efficiency. I am investigating the use of blocking layers in TEPLEDs to improve stability. ",
+                group: descriptionDesktop,
+                overflow: TextOverflow.clip,
+                stepGranularity: 1.0,
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.01,
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: InkWell(
+            onTap: () => launch(
+                "https://drive.google.com/file/d/1oE6N9I6cSaTvloNJWA2Y_tCyyROzbLQ8/view?usp=sharing"),
+            child: AutoSizeText(
+              "Full Poster",
+              stepGranularity: 1,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontFamily: "Poppins",
+                  decoration: TextDecoration.underline),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.07,
+        ),
+        Center(
+          child: AutoSizeText(
             "Summer 2021\nNCAR Earth Observing Laboratory Internship",
             group: titleGroupDesktop,
+            stepGranularity: 1,
             style: TextStyle(
               fontSize: 30,
               fontFamily: "Poppins",
@@ -41,7 +108,7 @@ class ProjectsDesktop extends StatelessWidget {
             Container(
               width: MediaQuery.of(context).size.width * 0.4,
               child: AutoSizeText(
-                "While working with the Integrated Surface Flux Systems (ISFS) team, I developed a software library for a TI ADC and analyzed readings for determining stability and temperature dependence. My efforts on the group's circuit board for converting analog sensor information to a digital format led to the replacement of the old board",
+                "While working with the Integrated Surface Flux Systems (ISFS) team, I developed a software library for a TI ADC and analyzed readings for determining stability and temperature dependence. My efforts led to the replacement of the old board. I presented my findings in a poster presentation at the end of the internship.",
                 group: descriptionDesktop,
                 overflow: TextOverflow.clip,
                 style: TextStyle(
@@ -55,7 +122,7 @@ class ProjectsDesktop extends StatelessWidget {
           ],
         ),
         SizedBox(
-          height: MediaQuery.of(context).size.height * 0.05,
+          height: MediaQuery.of(context).size.height * 0.01,
         ),
         Container(
           alignment: Alignment.center,
@@ -65,8 +132,9 @@ class ProjectsDesktop extends StatelessWidget {
                 "https://docs.google.com/file/d/1TDqsdvy7UHorm1uGunyxBrsKmqOhIKET/edit?usp=docslist_api&filetype=mspresentation"),
             child: AutoSizeText(
               "Full Poster",
-              presetFontSizes: [20, 15, 12],
+              stepGranularity: 1,
               style: TextStyle(
+                  fontSize: 20,
                   color: Colors.white,
                   fontFamily: "Poppins",
                   decoration: TextDecoration.underline),
@@ -74,12 +142,13 @@ class ProjectsDesktop extends StatelessWidget {
           ),
         ),
         SizedBox(
-          height: MediaQuery.of(context).size.height * 0.05,
+          height: MediaQuery.of(context).size.height * 0.07,
         ),
         Center(
           child: AutoSizeText(
             "Spring 2021\nRobotics and Autonomous Systems Lab",
             group: titleGroupDesktop,
+            stepGranularity: 1,
             style: TextStyle(
               fontSize: 30,
               fontFamily: "Poppins",
@@ -107,6 +176,8 @@ class ProjectsDesktop extends StatelessWidget {
                 "Over the course of a semester, I worked on developing autonomous hardware and software for a RC car\nStage 1 - Speed Control: Accomplished using Hall effect sensor and PID controller \nStage 2 - Track Navigation: Car centered on line using voltage levels from analog video \nStage 3 - Dynamic Navigation: PixyCam provided object detection and custom H-bridge allowed for reverse movement ",
                 group: descriptionDesktop,
                 overflow: TextOverflow.clip,
+                stepGranularity: 1.0,
+                maxLines: 12,
                 style: TextStyle(
                   fontFamily: "Poppins",
                   color: Colors.white,
@@ -116,6 +187,26 @@ class ProjectsDesktop extends StatelessWidget {
               ),
             ),
           ],
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.01,
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: InkWell(
+            onTap: () => launch(
+                "https://drive.google.com/file/d/19TfCtCOZykakoqS9qnRZ4IJUvNz0YJzH/view?usp=sharing"),
+            child: AutoSizeText(
+              "Navigation Demo",
+              stepGranularity: 1.0,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontFamily: "Poppins",
+                  decoration: TextDecoration.underline),
+            ),
+          ),
         ),
         SizedBox(
           height: MediaQuery.of(context).size.height * 0.10,
--- lib/screens/Projects/mobile.dart
+++ lib/screens/Projects/mobile.dart
@@ -2,7 +2,12 @@ import 'package:auto_size_text/auto_size_text.dart';
 import "package:flutter/material.dart";
 import 'package:url_launcher/url_launcher.dart';
 
-class ProjectsMobile extends StatelessWidget {
+class ProjectsMobile extends StatefulWidget {
+  @override
+  _ProjectsMobileState createState() => _ProjectsMobileState();
+}
+
+class _ProjectsMobileState extends State<ProjectsMobile> {
   var titleGroupMobile = AutoSizeGroup();
 
   @override
@@ -16,8 +21,64 @@ class ProjectsMobile extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height * 0.1,
           child: AutoSizeText(
+            "Senior Thesis\nStabilizing Metal Anodes in Perovskite LEDs",
+            stepGranularity: 1.0,
+            style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+            maxLines: 2,
+          ),
+        ),
+        Center(
+          child: Image(
+            image: AssetImage("assets/images/thesis_poster.png"),
+            width: MediaQuery.of(context).size.width * 0.9,
+            fit: BoxFit.fill,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: InkWell(
+            onTap: () => launch(
+                "https://drive.google.com/file/d/1oE6N9I6cSaTvloNJWA2Y_tCyyROzbLQ8/view?usp=sharing"),
+            child: AutoSizeText(
+              "Full Poster",
+              stepGranularity: 1.0,
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontFamily: "Poppins",
+                  decoration: TextDecoration.underline),
+            ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.4,
+          margin: EdgeInsets.all(5.0),
+          child: AutoSizeText(
+            "My senior thesis focuses on stabilizing and improving perovskite LEDS (PeLEDs). Bottom-emitting PeLEDS (BEPLEDS) are more stable than top-emitting PeLEDS (TEPLEDs) but have a lower theoretical efficiency. I am investigating the use of blocking layers in TEPLEDs to improve stability. ",
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              fontFamily: "Poppins",
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.1,
+          child: AutoSizeText(
             "Summer 2021\nNCAR EOL Internship",
             group: titleGroupMobile,
+            stepGranularity: 1.0,
             style: TextStyle(
                 fontSize: 25,
                 color: Colors.white,
@@ -42,28 +103,29 @@ class ProjectsMobile extends StatelessWidget {
                 "https://docs.google.com/file/d/1TDqsdvy7UHorm1uGunyxBrsKmqOhIKET/edit?usp=docslist_api&filetype=mspresentation"),
             child: AutoSizeText(
               "Full Poster",
-              presetFontSizes: [15, 12],
+              stepGranularity: 1.0,
               style: TextStyle(
+                  fontSize: 15,
                   color: Colors.white,
                   fontFamily: "Poppins",
                   decoration: TextDecoration.underline),
             ),
           ),
         ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.02,
-        ),
+        // SizedBox(
+        //   height: MediaQuery.of(context).size.height * 0.001,
+        // ),
         Container(
           width: MediaQuery.of(context).size.width * 0.4,
-          margin: EdgeInsets.all(8.0),
+          margin: EdgeInsets.all(5.0),
           child: AutoSizeText(
-            "While working with the Integrated Surface Flux Systems (ISFS) team, I developed a software library for a TI ADC and analyzed readings for determining stability and temperature dependence. My efforts on the group's circuit board for converting analog sensor information to a digital format led to the replacement of the old board",
+            "While working with the Integrated Surface Flux Systems (ISFS) team, I developed a software library for a TI ADC and analyzed readings for determining stability and temperature dependence. My efforts led to the replacement of the old board. I presented my findings in a poster presentation at the end of the internship.",
             overflow: TextOverflow.clip,
             style: TextStyle(
               fontFamily: "Poppins",
               color: Colors.white,
             ),
-            textAlign: TextAlign.center,
+            textAlign: TextAlign.left,
           ),
         ),
         SizedBox(
@@ -73,6 +135,7 @@ class ProjectsMobile extends StatelessWidget {
           child: AutoSizeText(
             "Spring 2021\nAutonomous Systems Lab",
             group: titleGroupMobile,
+            stepGranularity: 1.0,
             style: TextStyle(
               fontSize: 25,
               fontFamily: "Poppins",
@@ -80,6 +143,7 @@ class ProjectsMobile extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
             textAlign: TextAlign.center,
+            maxLines: 2,
           ),
         ),
         SizedBox(
@@ -93,16 +157,33 @@ class ProjectsMobile extends StatelessWidget {
           ),
         ),
         Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: InkWell(
+            onTap: () => launch(
+                "https://drive.google.com/file/d/19TfCtCOZykakoqS9qnRZ4IJUvNz0YJzH/view?usp=sharing"),
+            child: AutoSizeText(
+              "Navigation Demo",
+              stepGranularity: 1.0,
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontFamily: "Poppins",
+                  decoration: TextDecoration.underline),
+            ),
+          ),
+        ),
+        Container(
           width: MediaQuery.of(context).size.width * 0.4,
-          margin: EdgeInsets.all(8.0),
+          margin: EdgeInsets.all(5.0),
           child: AutoSizeText(
-            "Over the course of a semester, I worked on developing autonomous hardware and software for a RC car.\n Stage 1 - Speed Control: Accomplished using Hall effect sensor and PID controller.\n Stage 2 - Track Navigation: Car centered on line using voltage levels from analog video.\n Stage 3 - Dynamic Navigation: PixyCam provided object detection and custom H-bridge allowed for reverse movement.",
+            "Over the course of a semester, I worked on developing autonomous hardware and software for a RC car.\nStage 1 - Speed Control: Accomplished using Hall effect sensor and PID controller.\nStage 2 - Track Navigation: Car centered on line using voltage levels from analog video.\nStage 3 - Dynamic Navigation: PixyCam provided object detection and custom H-bridge allowed for reverse movement.",
             overflow: TextOverflow.clip,
             style: TextStyle(
               fontFamily: "Poppins",
               color: Colors.white,
             ),
-            textAlign: TextAlign.center,
+            textAlign: TextAlign.left,
           ),
         ),
         SizedBox(
