00afcd2d57fee17820485a8d6f6cdac55652dad2
Jack Scherlag <jack.scherlag@gmail.com>
2022-01-31 00:22:59 -0500

    Changed mobile,desktop boundary. Fixed overflow

--- lib/widgets/quick_facts.dart
+++ lib/widgets/quick_facts.dart
@@ -3,19 +3,24 @@ import "package:flutter/material.dart";
 import "package:percent_indicator/percent_indicator.dart";
 import 'package:url_launcher/url_launcher.dart';
 
-class QuickFacts extends StatelessWidget {
-  var factsGroup = AutoSizeGroup();
-
+class QuickFacts extends StatefulWidget {
   final BoxConstraints cts;
 
   QuickFacts(this.cts);
+
+  @override
+  _QuickFactsState createState() => _QuickFactsState();
+}
+
+class _QuickFactsState extends State<QuickFacts> {
+  var factsGroup = AutoSizeGroup();
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, cts) {
         // -------------------- mobile view -------------------------------
-        if (cts.maxWidth <= 600 || cts.maxHeight <= 600) {
+        if (cts.maxWidth <= 900 || cts.maxHeight <= 900) {
           return Column(
             children: [
               SizedBox(
@@ -51,11 +56,14 @@ class QuickFacts extends StatelessWidget {
                 center: InkWell(
                   child: AutoSizeText(
                     "1 US Patent",
+                    minFontSize: 5,
                     maxLines: 1,
+                    stepGranularity: 5,
                     group: factsGroup,
-                    presetFontSizes: [25, 20, 15, 10, 5],
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: "Poppins"),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Poppins",
+                        fontSize: 25),
                   ),
                   onTap: () => launch(
                       'https://patents.google.com/patent/US10488064B1/en'),
@@ -75,6 +83,7 @@ class QuickFacts extends StatelessWidget {
                 center: InkWell(
                   child: AutoSizeText(
                     "1 Paper Coauthored",
+                    minFontSize: 5,
                     maxLines: 1,
                     group: factsGroup,
                     presetFontSizes: [25, 20, 15, 10, 5],
@@ -95,73 +104,88 @@ class QuickFacts extends StatelessWidget {
               ),
             ],
           );
-          // --------------- desktop view -------------------------
         } else {
+          // --------------- desktop view -------------------------------------
           return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              CircularPercentIndicator(
-                backgroundColor: Colors.transparent,
-                radius: MediaQuery.of(context).size.width * 0.12,
-                center: InkWell(
-                  child: AutoSizeText(
-                    ">650 Flight Hours",
-                    maxLines: 1,
-                    group: factsGroup,
-                    presetFontSizes: [25, 20, 15, 10, 5],
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: "Poppins"),
+              Expanded(
+                child: CircularPercentIndicator(
+                  backgroundColor: Colors.transparent,
+                  radius: MediaQuery.of(context).size.width * 0.15,
+                  center: InkWell(
+                    child: AutoSizeText(
+                      ">650 Flight Hours",
+                      stepGranularity: 5.0,
+                      minFontSize: 5,
+                      maxLines: 1,
+                      group: factsGroup,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Poppins",
+                          fontSize: 25),
+                    ),
+                    onTap: () =>
+                        launch('https://www.youtube.com/watch?v=1HWufbhWjMI'),
                   ),
-                  onTap: () =>
-                      launch('https://www.youtube.com/watch?v=1HWufbhWjMI'),
+                  animation: true,
+                  animationDuration: 1200,
+                  startAngle: 0.0,
+                  percent: 1.0,
+                  progressColor: Colors.black,
                 ),
-                animation: true,
-                animationDuration: 1200,
-                startAngle: 0.0,
-                percent: 1.0,
-                progressColor: Colors.black,
               ),
-              CircularPercentIndicator(
-                backgroundColor: Colors.transparent,
-                radius: MediaQuery.of(context).size.width * 0.12,
-                center: InkWell(
-                  child: AutoSizeText(
-                    "1 US Patent",
-                    maxLines: 1,
-                    group: factsGroup,
-                    presetFontSizes: [25, 20, 15, 10, 5],
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: "Poppins"),
+              Expanded(
+                child: CircularPercentIndicator(
+                  backgroundColor: Colors.transparent,
+                  radius: MediaQuery.of(context).size.width * 0.15,
+                  center: InkWell(
+                    child: AutoSizeText(
+                      "1 US Patent",
+                      stepGranularity: 5.0,
+                      minFontSize: 5,
+                      maxLines: 1,
+                      group: factsGroup,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Poppins",
+                          fontSize: 25),
+                    ),
+                    onTap: () => launch(
+                        'https://patents.google.com/patent/US10488064B1/en'),
                   ),
-                  onTap: () => launch(
-                      'https://patents.google.com/patent/US10488064B1/en'),
+                  animation: true,
+                  animationDuration: 1200,
+                  startAngle: 0.0,
+                  percent: 1.0,
+                  progressColor: Colors.black,
                 ),
-                animation: true,
-                animationDuration: 1200,
-                startAngle: 0.0,
-                percent: 1.0,
-                progressColor: Colors.black,
               ),
-              CircularPercentIndicator(
-                backgroundColor: Colors.transparent,
-                radius: MediaQuery.of(context).size.width * 0.12,
-                center: InkWell(
-                  child: AutoSizeText(
-                    "1 Paper Coauthored",
-                    maxLines: 1,
-                    group: factsGroup,
-                    presetFontSizes: [25, 20, 15, 10, 5],
-                    style:
-                        TextStyle(color: Colors.white, fontFamily: "Poppins"),
+              Expanded(
+                child: CircularPercentIndicator(
+                  backgroundColor: Colors.transparent,
+                  radius: MediaQuery.of(context).size.width * 0.15,
+                  center: InkWell(
+                    child: AutoSizeText(
+                      "1 Paper Coauthored",
+                      maxLines: 1,
+                      stepGranularity: 5.0,
+                      minFontSize: 5,
+                      group: factsGroup,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Poppins",
+                          fontSize: 25),
+                    ),
+                    onTap: () => launch(
+                        'https://www.semanticscholar.org/paper/Seeds-as-a-Source-of-Plant-Inhibitory-Fungi%3A-for-Scherlag-Scherlag/64098cbc8fe4a3b1c865b0f14420fa0c689725be'),
                   ),
-                  onTap: () => launch(
-                      'https://www.semanticscholar.org/paper/Seeds-as-a-Source-of-Plant-Inhibitory-Fungi%3A-for-Scherlag-Scherlag/64098cbc8fe4a3b1c865b0f14420fa0c689725be'),
+                  animation: true,
+                  animationDuration: 1200,
+                  startAngle: 0.0,
+                  percent: 1.0,
+                  progressColor: Colors.black,
                 ),
-                animation: true,
-                animationDuration: 1200,
-                startAngle: 0.0,
-                percent: 1.0,
-                progressColor: Colors.black,
               )
             ],
           );
