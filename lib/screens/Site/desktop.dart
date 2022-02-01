49386f0c98650ce5a89113ee0b6c84a56d5b5cfa
Jack Scherlag <jack.scherlag@gmail.com>
2022-02-01 11:39:18 -0500

    Transitioning over to listview

--- lib/screens/Site/desktop.dart
+++ lib/screens/Site/desktop.dart
@@ -4,13 +4,23 @@ import "package:flutter/material.dart";
 class SiteDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AutoSizeText(
-        "This website was built with Flutter and is hosted on Firebase",
-        style:
-            TextStyle(fontSize: 30, fontFamily: "Poppins", color: Colors.white),
-        textAlign: TextAlign.center,
-      ),
+    return ListView(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+        ),
+        Center(
+          child: AutoSizeText(
+            "This website was built with Flutter and is hosted on Firebase",
+            style: TextStyle(
+              fontSize: 30,
+              fontFamily: "Poppins",
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
     );
   }
 }
