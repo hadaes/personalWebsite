e154df4dacdab4d412cffefc39f7a8e73e41ac9f
Jack Scherlag <jack.scherlag@gmail.com>
2022-02-03 14:43:10 -0500

    Add mobile content

--- lib/screens/Site/mobile.dart
+++ lib/screens/Site/mobile.dart
@@ -4,16 +4,71 @@ import "package:flutter/material.dart";
 class SiteMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
-      child: Center(
-        child: AutoSizeText(
-          "This website was built with Flutter and is hosted on Firebase",
-          presetFontSizes: [25, 20, 15, 10],
-          style: TextStyle(color: Colors.white, fontFamily: "Poppins"),
-          textAlign: TextAlign.center,
+    return ListView(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
         ),
-      ),
+        Center(
+          child: AutoSizeText(
+            "This website was built with Flutter and is hosted on Firebase",
+            presetFontSizes: [25, 20, 15, 10],
+            style: TextStyle(color: Colors.white, fontFamily: "Poppins"),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.07,
+        ),
+        Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: AutoSizeText(
+                  "Early Concepts",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Image(
+                      image: AssetImage(
+                        "assets/images/primary.jpeg",
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Image(
+                      image: AssetImage(
+                        "assets/images/primary.jpeg",
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
