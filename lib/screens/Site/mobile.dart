94ae63becd154b2b64c9db4fc89022bf7fa6a3ce
Jack Scherlag <jack.scherlag@gmail.com>
2022-02-06 20:59:19 -0500

    Text adjustmenjts

--- lib/screens/Site/mobile.dart
+++ lib/screens/Site/mobile.dart
@@ -10,30 +10,44 @@ class SiteMobile extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).size.height * 0.05,
         ),
-        Center(
-          child: AutoSizeText(
-            "This website was built with Flutter and is hosted on Firebase",
-            presetFontSizes: [25, 20, 15, 10],
-            style: TextStyle(color: Colors.white, fontFamily: "Poppins"),
-            textAlign: TextAlign.center,
+        Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.1,
+          padding: EdgeInsets.symmetric(
+            horizontal: 5.0,
+          ),
+          child: Center(
+            child: AutoSizeText(
+              "This website was built with Flutter and is hosted on Firebase",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: "Poppins",
+                fontSize: 25,
+              ),
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
         SizedBox(
-          height: MediaQuery.of(context).size.height * 0.07,
+          height: MediaQuery.of(context).size.height * 0.05,
         ),
         Container(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Center(
-                child: AutoSizeText(
-                  "Early Concepts",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: "Poppins",
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  child: AutoSizeText(
+                    "Early Concepts",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "Poppins",
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
@@ -72,15 +86,19 @@ class SiteMobile extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).size.height * 0.10,
         ),
-        Center(
-          child: AutoSizeText(
-            "Timeline",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: "Poppins",
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+        Container(
+          width: MediaQuery.of(context).size.width * 0.75,
+          height: MediaQuery.of(context).size.height * 0.05,
+          child: Center(
+            child: AutoSizeText(
+              "Timeline",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: "Poppins",
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
